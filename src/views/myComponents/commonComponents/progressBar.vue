cba75e462f85bce0e5bf802d92588a80e305fb90
lgy <lgy-lgy@qq.com>
2021-04-03 19:06:32 +0800

    feat:常用组件之进度条

--- src/views/myComponents/commonComponents/progressBar.vue
+++ src/views/myComponents/commonComponents/progressBar.vue
@@ -1,16 +1,101 @@
 <template>
   <div>
     <div class="g-home-module">
-    进度条。。。
+      <div class="m-item" style="align-items: normal">
+        <el-progress :percentage="rate"></el-progress>
+      </div>
+      <div class="m-item" style="align-items: normal">
+        <el-progress
+          :text-inside="true"
+          :stroke-width="26"
+          :percentage="rate"
+        ></el-progress>
+      </div>
+
+      <div class="m-item">
+        <el-progress type="circle" :percentage="rate"></el-progress>
+      </div>
+      <div class="m-item">
+        <el-progress type="dashboard" :percentage="rate"></el-progress>
+      </div>
+      <div class="m-item">
+        <div class="liPerson">
+          <div class="liPersonBar">
+            <div
+              :style="{
+                width: rate + '%',
+                paddingLeft: rate + '%',
+              }"
+            >
+              <span> {{ rate }}% </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="m-item">
+        <div class="progressBar">
+          <div class="progress" :style="{ width: rate + '%' }"></div>
+          <span class="in-text" :style="{ left: rate + '%' }">{{ rate + "%" }}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      rate: 33,
+    };
   },
   methods: {},
 };
 </script>
-<style></style>
+<style scoped lang="less">
+.liPerson {
+  width: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .liPersonBar {
+    border: 1px solid aqua;
+    // padding: 0 0.3vh;
+    margin-right: 3%;
+    border-radius: 5px;
+    height: 2.5vh;
+    width: 80%;
+    position: relative;
+    display: flex;
+    align-items: center;
+
+    div {
+      border-radius: 5px;
+      height: 70%;
+      background-color: #1bc3dc;
+      position: absolute;
+      display: flex;
+      align-items: center;
+      transition: all 0.5s ease;
+    }
+  }
+}
+
+.progressBar {
+  width: 200px;
+  height: 20px;
+  border: 1px solid transparent;
+  border-radius: 10px;
+  text-align: left;
+  .progress {
+    height: 20px;
+    background: aqua;
+    border-radius: 10px;
+  }
+  .in-text {
+    position: relative;
+    bottom: 19px;
+  }
+}
+</style>
--- src/views/myComponents/commonComponents/promptBox.vue
+++ src/views/myComponents/commonComponents/promptBox.vue
@@ -1,16 +1,70 @@
 <template>
   <div>
     <div class="g-home-module">
-
+      <div class="m-item" @click="showHardTip()">普通软提示</div>
+      <div class="m-item"></div>
+      <div class="m-item"></div>
+    </div>
+    <div class="hardTip-bg">
+      <div class="hardTip">
+        硬提示内容
+        <button class="btn">关闭提示</button>
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      hardTip: false,
+    };
   },
-  methods: {},
+  methods: {
+    showHardTip() {
+      this.hardTip = true;
+    },
+    showSoftTip(showTime) {
+      this.setInterval(() => {
+        this.hardTip = false;
+      }, 1000 * showTime);
+    },
+  },
 };
 </script>
-<style></style>
+<style scoped lang="less">
+.hardTip-bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+  background-color: rgba(0, 0, 0, 0.5);
+  .hardTip {
+    background-color: white;
+    overflow: hidden;
+    position: fixed;
+    top: 30%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-color: #80ffff;
+    border-style: solid;
+    border-width: 1px;
+    border-radius: 5px;
+    height: 200px;
+    width: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    .btn {
+      background-color: #6fe7f1;
+      border: solid;
+      border-color: transparent;
+      height: 30%;
+      width:100%;
+    }
+  }
+}
+</style>
