15a189bcaa9a2693dd5730ab585abe2d6019afd9
“lgy <lgy-lgy@qq.com>
2021-10-17 18:08:47 +0800

    fix：将埋点监听点击方式改为不会被覆盖的写法

--- src/assets/js/umami/umami.js
+++ src/assets/js/umami/umami.js
@@ -1,7 +1,7 @@
 const umamiConfig = require('./umami-config') //埋点跟踪代码配置
 
 // 根据环境加载埋点跟踪代码信息配置
-let dataWebsiteId="";
+let dataWebsiteId = "";
 if (process.env.NODE_ENV === 'production') {
   dataWebsiteId = umamiConfig.dataWebsiteIdProduction;
 } else {
@@ -12,36 +12,33 @@ if (process.env.NODE_ENV === 'production') {
 document.write('<script async defer data-website-id="' + dataWebsiteId + '" src="' + umamiConfig.umamiSrc + '"><\/script>');
 
 
-window.onload = function () {
-  document.onmousedown = function (event) //鼠标按下事件
-  {
-    if (event.path) {
-      let event_value = "",
-        event_type = "click";
-      let cur = event.path[0]
-      if (cur) {
-        if (cur.innerText) //直接点了带文字的元素
-          event_value = cur.innerText;
-        else if (cur.alt) //alt有值的img标签
-          event_value = cur.alt;
-        else {
-          //往外查询是否点击到a标签
-          for (let i = 1, curItem = event.path[i]; curItem; i++) {
-            if (curItem.tagName == "A") {
-              cur = curItem;
-              break;
-            }
-            curItem = event.path[i + 1];
+document.addEventListener("click", event => {
+  if (event.path) {
+    let event_value = "",
+      event_type = "click";
+    let cur = event.path[0]
+    if (cur) {
+      if (cur.innerText) //直接点了带文字的元素
+        event_value = cur.innerText;
+      else if (cur.alt) //alt有值的img标签
+        event_value = cur.alt;
+      else {
+        //往外查询是否点击到a标签
+        for (let i = 1, curItem = event.path[i]; curItem; i++) {
+          if (curItem.tagName == "A") {
+            cur = curItem;
+            break;
           }
-
-          if (cur.innerText) event_value = cur.innerText; //若a标签内有值取a标签的值
-          else
-            event_value = "点击位置未设值";
+          curItem = event.path[i + 1];
         }
-      }
 
-      umami.trackEvent(event_value, event_type);
+        if (cur.innerText) event_value = cur.innerText; //若a标签内有值取a标签的值
+        else
+          event_value = "点击位置未设值";
+      }
     }
 
+    umami.trackEvent(event_value, event_type);
   }
-}
+
+})
