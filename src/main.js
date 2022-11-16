dccb8aab169d42e20cd5424d6fcf405088aaeded
“lgy <lgy-lgy@qq.com>
2022-11-17 00:12:40 +0800

    add:重写message提示,消除重复提示

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
  * @Autor: lgy
  * @Date: 2022-10-07 17:24:28
  * @LastEditors: lgy
- * @LastEditTime: 2022-10-09 23:03:46
+ * @LastEditTime: 2022-11-17 00:10:35
  */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
@@ -45,6 +45,13 @@ Vue.config.productionTip = false
 Vue.use(ElementUI, {
   i18n: (key, value) => i18n.t(key, value)
 });
+
+import {
+  message
+} from '@/assets/js/expand/resetMessage.js' //重写message提示
+Vue.prototype.$message = message
+
+
 Vue.use(DrawStartsUI)
 Vue.component('Navigation', Navigation)
 Vue.component('Myfooter', Myfooter)
