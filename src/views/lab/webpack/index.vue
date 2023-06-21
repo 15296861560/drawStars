9987944c47ab8ad08d359891b60b2a36f4e6ce0a
“lgy <lgy-lgy@qq.com>
2023-06-21 22:43:30 +0800

    fix:修复webpack打包例子

--- src/views/lab/webpack/index.vue
+++ src/views/lab/webpack/index.vue
@@ -1,21 +1,31 @@
+<!--
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: lgy
+ * @Date: 2022-12-24 14:01:34
+ * @LastEditors: lgy
+ * @LastEditTime: 2023-06-21 22:41:36
+-->
+<template>
+  <h1>webpack简单示例:控制台查看输出结果</h1>
+</template>
 <script>
-  (function (list) {
-    function require(file) {
-      let exports = {};
-      (function (exports, code) {
-        eval(code)
-      })(exports, list[file])
-      return exports;
-    }
+(function (list) {
+  function requireFun(file) {
+    let exportsObj = {};
+    (function (exportsObj, code) {
+      eval(code);
+    })(exportsObj, list[file]);
+    return exportsObj;
+  }
 
-    require("index.js");
-  })({
-    'index.js': `var add = require('add.js').default
-    var print = require('print.js').default
+  requireFun("index.js");
+})({
+  "index.js": `var add = requireFun('add.js').default
+    var print = requireFun('print.js').default
   console.log(add(1, 2))
   print("lgy")`,
-    'add.js': `exports.default = function(a,b){return a + b}`,
-    'print.js':`exports.default=function(item){console.log(item)}`
-  })
-
+  "add.js": `exportsObj.default = function(a,b){return a + b}`,
+  "print.js": `exportsObj.default=function(item){console.log(item)}`,
+});
 </script>
