0cc783d924516610f9451729e566d13fc24d2123
“lgy <lgy-lgy@qq.com>
2023-07-22 23:18:57 +0800

    style:消除告警,调整格式

--- src/stores/api-info.ts
+++ src/stores/api-info.ts
@@ -4,21 +4,22 @@
  * @Autor: lgy
  * @Date: 2022-11-27 01:11:05
  * @LastEditors: lgy
- * @LastEditTime: 2022-12-23 23:59:51
+ * @LastEditTime: 2023-07-22 23:16:58
  */
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 
 export const apiInfoStore = defineStore("apiInfo", () => {
-  
-  const url=ref('/api')
-  const getURL=computed(()=>url)
-  function changeApi(newVal:string) {
-    url.value=newVal;
+
+  const url = ref('/api')
+  const getURL = computed(() => url)
+  function changeApi(newVal: string) {
+    url.value = newVal;
   }
 
-  return { url, getURL,changeApi }
+  return { url, getURL, changeApi }
 }, {
   persist: {
     storage: sessionStorage
-  }});
+  }
+});
